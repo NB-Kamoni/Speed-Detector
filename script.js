95fc7e703b2bad21416f28d0f67227401a5d5b44
NB-Kamoni <kamoninjoki@gmail.com>
2024-03-26 14:24:11 +0300

    Updates readme file

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
+// Adds an event listener to the form submit event
 document.getElementById('speedForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    
+     // Gets the input value
     const speed = parseInt(document.getElementById('speedInput').value);
     const speedLimit = 70;
     const demeritPointsPerKmOverLimit = 1;
     const pointsThresholdForSuspension = 12;
     
     let demeritPoints = 0;
-  
+    // Calculates demerit points if speed is above the limit
     if (speed > speedLimit) {
       const kmOverLimit = speed - speedLimit;
       demeritPoints = Math.floor(kmOverLimit / 5) * demeritPointsPerKmOverLimit;
     }
-  
+    // Generates status based on demerit points
     let resultMessage = '';
     if (demeritPoints > 0) {
       resultMessage = `Dimerit Points: ${demeritPoints}`;
